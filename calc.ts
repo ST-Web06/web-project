471624f28a40632e423c09645e3677529364e7dd
Temur2006 <salnikovtemur2006@gmail.com>
2025-09-11 10:02:53 +0300

    Es Linter(2.1) + Calc(1.3)

---
+++ calc.ts
@@ -0,0 +1,45 @@
+function calc(input: string): string{
+    let data: string[] = input.replaceAll('(', " ").replaceAll(')', " ").split(" ");
+    let stack: number[] = [];
+    for (let i = data.length - 1; i >= 0; i--) {
+        let element = data[i];
+        if (element == " " || element == '') {
+            continue;
+        }
+        if (!isNaN(Number(element))) {
+            stack.push(Number(element));
+        } else {
+            let firstNum = stack.pop();
+            let secNum = stack.pop();
+
+            if (firstNum === undefined || secNum === undefined) {
+                return("Недостаточно операндов для операции");
+            }
+
+            switch (element) {
+                case "+":
+                    stack.push(firstNum + secNum);
+                    break;
+                case "-":
+                    stack.push(firstNum - secNum);
+                    break;
+                case "*":
+                    stack.push(firstNum * secNum);
+                    break;
+                case "/":
+                    stack.push(firstNum / secNum);
+                    break;
+                default: {
+                    return(`Неизвестный оператор: ${element}`);
+                }
+            }
+        }
+    }
+    const result = stack.pop();
+    if (result === undefined || stack.length > 0) {
+        return("Некорректное выражение");
+    }
+    return String(result);
+}
+
+console.log(`Результат: ${calc("* - (33.5 -2 - 6 4")}`);
